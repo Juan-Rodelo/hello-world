7289902ffab5d76baa6f8f6413bd7b8bde67e3a8
Juan-Rodelo <juancamilo849@gmail.com>
2021-08-12 16:09:08 -0500

    First Commit

---
+++ components/Chat.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default class Chat extends React.Component {
+  render() {
+    let name = this.props.route.params.name; // OR ...
+    // let { name } = this.props.route.params;
+    this.props.navigation.setOptions({ title: name });
+    return (
+
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Hello Screen2!</Text>
+        <Button
+          title="go to Start"
+          onPress={() => this.props.navigation.navigate("Start")}
+        />
+      </View>
+    )
+  }
+}
